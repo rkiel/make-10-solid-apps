b99c64eb61454a26ede637b1f0c5fe2def7457fd
rkiel <cygnus@pobox.com>
2022-11-19 18:53:37 -0500

    moving: basic canvas movement

---
+++ moving/src/App.jsx
@@ -0,0 +1,54 @@
+import { onMount, createEffect, createSignal } from "solid-js";
+import "./App.css";
+
+function App() {
+  const [x, setX] = createSignal(0);
+  const [y, setY] = createSignal(0);
+
+  let canvas;
+
+  onMount(() => {
+    const context = canvas.getContext("2d");
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+  });
+  createEffect(() => {
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, window.innerHeight, window.innerWidth);
+    context.fillRect(x(), y(), 100, 100);
+  });
+
+  function moveUp() {
+    setY(y() - 20);
+  }
+  function moveDown() {
+    setY(y() + 20);
+  }
+  function moveLeft() {
+    setX(x() - 20);
+  }
+  function moveRight() {
+    setX(x() + 20);
+  }
+  return (
+    <div className="app">
+      <canvas ref={canvas} />
+
+      <div className="arrows">
+        <button onClick={moveUp}>Up</button>
+        <button onClick={moveLeft}>Left</button>
+        <button onClick={moveDown}>Down</button>
+        <button onClick={moveRight}>Right</button>
+      </div>
+
+      <div className="images">
+        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
+        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
+        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
