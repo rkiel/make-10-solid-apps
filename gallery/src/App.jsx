3f8d4458829ae69ce44ab928c2b9cd103ad1ce50
rkiel <cygnus@pobox.com>
2022-11-25 14:29:08 -0500

    gallery: grab images and display

--- gallery/src/App.jsx
+++ gallery/src/App.jsx
@@ -1,6 +1,19 @@
+import { createResource } from "solid-js";
+
 import "./App.css";
 
+const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+
+async function fetchData() {
+  const response = await fetch(
+    `https://api.unsplash.com/photos/?client_id=${accessKey}`
+  );
+  return await response.json();
+}
+
 function App() {
+  const [images] = createResource(fetchData);
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
@@ -11,11 +24,13 @@ function App() {
       </form>
 
       <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
-          <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
-          </div>
-        ))}
+        <For each={images()} fallback={<div>Loading...</div>}>
+          {(image, index) => (
+            <div className="image" key={index}>
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </div>
+          )}
+        </For>
       </div>
     </div>
   );
