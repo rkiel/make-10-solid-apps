8dca20187f3e13d5d49f5b93f20757a2bb1b03b9
rkiel <cygnus@pobox.com>
2022-11-25 17:04:25 -0500

    gallery: fetch page at a time

--- gallery/src/App.jsx
+++ gallery/src/App.jsx
@@ -1,18 +1,44 @@
-import { createResource } from "solid-js";
+import { createSignal, createEffect } from "solid-js";
 
 import "./App.css";
 
 const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
 
-async function fetchData() {
+async function getPhotos(p) {
   const response = await fetch(
-    `https://api.unsplash.com/photos/?client_id=${accessKey}`
+    `https://api.unsplash.com/photos/?client_id=${accessKey}&page=${p}`
   );
   return await response.json();
 }
 
+function imageTag(image, index) {
+  return (
+    <a
+      className="image"
+      key={index}
+      href={image.links.html}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={image.urls.regular} alt={image.alt_description} />
+    </a>
+  );
+}
+
+const [page, setPage] = createSignal(1);
+const [images, setImages] = createSignal([]);
+
+function onMore(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  setPage((prev) => prev + 1);
+}
+
 function App() {
-  const [images] = createResource(fetchData);
+  createEffect(async () => {
+    const json = await getPhotos(page());
+    setImages((prev) => prev.concat(json));
+  });
 
   return (
     <div className="app">
@@ -21,15 +47,12 @@ function App() {
       <form>
         <input type="text" placeholder="Search Unsplash..." />
         <button>Search</button>
+        <button onClick={onMore}>More ({images().length})</button>
       </form>
 
       <div className="image-grid">
         <For each={images()} fallback={<div>Loading...</div>}>
-          {(image, index) => (
-            <div className="image" key={index}>
-              <img src={image.urls.regular} alt={image.alt_description} />
-            </div>
-          )}
+          {imageTag}
         </For>
       </div>
     </div>
