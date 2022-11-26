8b61007d5c555e03f51e2cd2e1dea0691ee28e1e
rkiel <cygnus@pobox.com>
2022-11-26 16:19:54 -0500

    trivia: initial

--- trivia/src/components/CategorySelector.jsx
+++ trivia/src/components/CategorySelector.jsx
@@ -1,15 +1,19 @@
 import categories from "../categories";
 
+function optionTag(category, index) {
+  return (
+    <option key={index} value={category.id}>
+      {category.name}
+    </option>
+  );
+}
+
 export default function CategorySelector() {
   return (
     <div className="category-selector">
       <p>Select Category</p>
       <select>
-        {categories.map((category, index) => (
-          <option key={index} value={category.id}>
-            {category.name}
-          </option>
-        ))}
+        <For each={categories}>{optionTag}</For>
       </select>
     </div>
   );
--- trivia/src/components/Question.jsx
+++ trivia/src/components/Question.jsx
@@ -1,13 +1,13 @@
 const sampleAnswers = ["One", "Two", "Three", "Four"];
 
+function buttonTag(answer, index) {
+  return <button key={index}>{answer}</button>;
+}
 export default function Question() {
   return (
     <div className="question">
       <h2>Question Here</h2>
-
-      {sampleAnswers.map((answer, index) => (
-        <button key={index}>answer</button>
-      ))}
+      <For each={sampleAnswers}>{buttonTag}</For>
     </div>
   );
 }
