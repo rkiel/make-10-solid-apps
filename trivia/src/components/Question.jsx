c39a7cc06dc186bf00047e0853ed35b37332ac27
rkiel <cygnus@pobox.com>
2022-11-26 17:55:46 -0500

    trivia: cleanup

--- trivia/src/components/Question.jsx
+++ trivia/src/components/Question.jsx
@@ -3,16 +3,18 @@ import shuffle from "lodash.shuffle";
 function buttonTag(answer, index) {
   return <button key={index} innerHTML={answer} />;
 }
+
+function answers(q) {
+  return shuffle([q.correct_answer].concat(q.incorrect_answers));
+}
+
 export default function Question(props) {
   const question = () => props.question;
-
-  const answers = () =>
-    shuffle([question().correct_answer].concat(question().incorrect_answers));
 
   return (
     <div className="question">
       <h2 innerHTML={question().question} />
-      <For each={answers()}>{buttonTag}</For>
+      <For each={answers(question())}>{buttonTag}</For>
     </div>
   );
 }
